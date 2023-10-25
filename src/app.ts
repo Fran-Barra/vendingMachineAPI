5147838c580801f085844188c9128b3fef8a3b9d
Franco B <franco.barravecchia2002@gmail.com>
2023-10-25 11:11:34 -0300

    update: MachineController integration

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ export class App{
 
     private initialiseDatabaseConnection(): void{
         const { MONGO_USER, MONGO_PASSWORD, MONGO_PATH} = process.env;
-        mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`);
+        mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_PATH}`);
     }
 
     private initializeMiddleware(): void {
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import 'dotenv/config';
 import validateEnv from 'servicies/validateEnv';
 import {App} from './app';
+import { MachineController } from 'controller/machineController';
 
 
 validateEnv();
-const app = new App([], Number(process.env.PORT));
+const app = new App([
+    new MachineController()
+], Number(process.env.PORT));
 app.listen();
