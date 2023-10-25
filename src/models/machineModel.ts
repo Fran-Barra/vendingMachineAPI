95a8aae973e151841866ab2e67ef7623486fd130
Franco B <franco.barravecchia2002@gmail.com>
2023-10-25 11:09:18 -0300

    feat: MachineModel

---
+++ src/models/machineModel.ts
@@ -0,0 +1,21 @@
+import { Schema, model } from "mongoose"
+
+//TODO: make the paramaeter requiered
+export interface IMachine {
+    readonly status: MachineStatus,
+    readonly credit: number
+}
+
+export enum MachineStatus{
+    ON,
+    OUT_OF_SERVICE,
+    ON_MAINTENANACE
+}
+
+//TODO: make status requiered
+const machineSchema = new Schema<IMachine>({
+    status: MachineStatus,
+    credit: {type: Number, required: true}
+})
+
+export default model<IMachine>('Machine', machineSchema);
