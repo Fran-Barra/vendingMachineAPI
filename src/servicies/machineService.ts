175887eef919dbb7e8f200ce9a3847b6d04b403e
Franco B <franco.barravecchia2002@gmail.com>
2023-10-25 11:10:50 -0300

    feat: MachineService

---
+++ src/servicies/machineService.ts
@@ -0,0 +1,17 @@
+import { MachineFactory } from 'factory.ts/machineFactory';
+import { MachineRepository } from 'repository/machineRepository';
+
+
+export class MachineService {
+
+    public static createAndSave(machineDTO: any): any {
+        try {
+            const machine = MachineFactory.createMachine(machineDTO);
+            MachineRepository.save(machine);
+            return machine;
+        }catch (err) {
+            throw new Error("Unacble to create machine");
+            //TODO: handle correctly this
+        }
+    }
+}
