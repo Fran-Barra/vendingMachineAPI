7aac5df9b7b1786c6b52385bfd86badd29bcc447
Franco B <franco.barravecchia2002@gmail.com>
2023-11-01 10:34:12 -0300

    update: stock restriction

--- src/models/machineProductModel.ts
+++ src/models/machineProductModel.ts
@@ -9,7 +9,7 @@ export interface IMachineProduct {
 const machineProductSchema = new Schema<IMachineProduct>({
   machine: { type: Schema.Types.ObjectId, ref: "Machine", required: true },
   product: { type: Schema.Types.ObjectId, ref: "Product", required: true },
-  stock: { type: Number, default: 0 },
+  stock: { type: Number, default: 0, min: 0 },
 });
 
 export default model<IMachineProduct>("MachineProduct", machineProductSchema);
