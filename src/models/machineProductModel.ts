ecb76f10aa162ac4176f9dbe4f212147333ee158
Franco B <franco.barravecchia2002@gmail.com>
2023-11-01 10:52:33 -0300

    update: add name to product

--- src/models/machineProductModel.ts
+++ src/models/machineProductModel.ts
@@ -3,12 +3,14 @@ import { Schema, model, Types } from "mongoose";
 export interface IMachineProduct {
   machine: Types.ObjectId;
   product: Types.ObjectId;
+  name: String;
   stock: number;
 }
 
 const machineProductSchema = new Schema<IMachineProduct>({
   machine: { type: Schema.Types.ObjectId, ref: "Machine", required: true },
   product: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+  name: {type: String, required: true},
   stock: { type: Number, default: 0, min: 0 },
 });
 
