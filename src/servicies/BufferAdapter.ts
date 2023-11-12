d1a5238308acbb0b2cec20d39297e3aa1ac51943
Franco B <franco.barravecchia2002@gmail.com>
2023-11-12 12:05:48 -0300

    fix: don't destroy server on error

--- src/servicies/BufferAdapter.ts
+++ src/servicies/BufferAdapter.ts
@@ -13,58 +13,77 @@ export class BufferAdapter{
     }
 
     public updateStatus(message: Buffer){
-        const values: {status: number, machineId: String} = JSON.parse(message.toString())
+        try {
+            const values: {status: number, machineId: String} = JSON.parse(message.toString())
 
-        const newStatus: number = Number(values.status);
-        if (isNaN(newStatus)) throw Error('wrong message send by mqtt')
+            const newStatus: number = Number(values.status);
+            if (isNaN(newStatus)) throw Error('wrong message send by mqtt')
 
-        const id: String = values.machineId;
-        if (id === undefined) throw Error('wrong message send by mqtt, id missing')
-        MachineRepository.modifyMachineStatus(id, newStatus)
+            const id: String = values.machineId;
+            if (id === undefined) throw Error('wrong message send by mqtt, id missing')
+            MachineRepository.modifyMachineStatus(id, newStatus)
+        } catch (err){
+            console.log(err)
+        }
     }
 
     public updateStock(message: Buffer){
-        const values:{machineId: string, stock: string} = JSON.parse(message.toString())
+        try {
+            const values:{machineId: string, stock: string} = JSON.parse(message.toString())
 
-        const newAmount: number = Number(values.stock);
-        if (isNaN(newAmount)) throw Error('wrong message send by mqtt')
+            const newAmount: number = Number(values.stock);
+            if (isNaN(newAmount)) throw Error('wrong message send by mqtt')
 
-        const id: String  | undefined = values.machineId;
-        if (id === undefined) throw Error('wrong message send by mqtt, id missing')
-        MachineRepository.modifyMachineStock(id, newAmount)
+            const id: String  | undefined = values.machineId;
+            if (id === undefined) throw Error('wrong message send by mqtt, id missing')
+            MachineRepository.modifyMachineStock(id, newAmount)
+        } catch (err){
+            console.log(err)
+        }
     }
 
     public boughtProduct(message: Buffer) {
-        const values: {productId: string, machineId: string}= JSON.parse(message.toString())
+        try {
+            const values: {productId: string, machineId: string}= JSON.parse(message.toString())
 
+            const machineId: String  | undefined = values.machineId;
+            if (machineId === undefined) throw Error('wrong message send by mqtt, mid missing');
 
-        const machineId: String  | undefined = values.machineId;
-        if (machineId === undefined) throw Error('wrong message send by mqtt, mid missing');
+            const productId: String  | undefined = values.productId;
+            if (productId === undefined) throw Error('wrong message send by mqtt, pid missing');
 
-        const productId: String  | undefined = values.productId;
-        if (productId === undefined) throw Error('wrong message send by mqtt, pid missing');
-
-        MachineService.BoughtProductOfMachine(machineId, productId);
+            MachineService.BoughtProductOfMachine(machineId, productId);
+        } catch (err){
+            console.log(err)
+        }
     }
 
     public updateCredit(message: Buffer) {
-        const values: {increment: string, machineId: string}= JSON.parse(message.toString())
+        try {
+            const values: {increment: string, machineId: string}= JSON.parse(message.toString())
 
-        const machineId: String  | undefined = values.machineId;
-        if (machineId === undefined) throw Error('wrong message send by mqtt, id missing');
+            const machineId: String  | undefined = values.machineId;
+            if (machineId === undefined) throw Error('wrong message send by mqtt, id missing');
 
-        const creditAmountString: String | undefined = values.increment;
-        if (creditAmountString === undefined) throw Error('wrong message send by mqtt, amount missing');
-        const creditAmount: number = Number(creditAmountString);
-        if (isNaN(creditAmount)) throw Error(`Not valid credit amount: ${creditAmountString}`)
+            const creditAmountString: String | undefined = values.increment;
+            if (creditAmountString === undefined) throw Error('wrong message send by mqtt, amount missing');
+            const creditAmount: number = Number(creditAmountString);
+            if (isNaN(creditAmount)) throw Error(`Not valid credit amount: ${creditAmountString}`)
 
-        MachineRepository.increaseOrDecreaseCreditOfMachine(machineId, creditAmount);
+            MachineRepository.increaseOrDecreaseCreditOfMachine(machineId, creditAmount);
+        } catch (err){
+            console.log(err)
+        }
     }
 
     public async initializeMachine(message: Buffer) {
-        const machienId = message.toString();
+        try {
+            const machienId = message.toString();
 
-        const data: MachineInitData = await MachineFactory.getMachineInitData(machienId)
-        this.mqttClient.sendMessage(`/machine_init_${machienId}`, data)
+            const data: MachineInitData = await MachineFactory.getMachineInitData(machienId)
+            this.mqttClient.sendMessage(`/machine_init_${machienId}`, data)
+        } catch (err){
+            console.log(err)
+        }
     }
 }
